404394466f244134f523a15994c31422c84528dd
nickarthur <nickarthur.night@gmail.com>
2020-10-25 01:21:11 -0500

    Projects setup. 0. Created window in Scene View Delegate 1. Added images to xcassets 2. Added UITabBarController 3. Added SearchVC and FavoritesVC

---
+++ GHFollowerrs/FavoritesVC.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritesVC.swift
+//  GHFollowerrs
+//
+//  Created by Larry Nickerson on 10/25/20.
+//
+
+import UIKit
+
+class FavoritesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+    
+
+
+
+}
---
+++ GHFollowerrs/SearchVC.swift
@@ -0,0 +1,19 @@
+//
+//  SearchVC.swift
+//  GHFollowerrs
+//
+//  Created by Larry Nickerson on 10/25/20.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+    
+
+
+}
