2b46e68da8ff8a465532265395c9688dd69f152b
nickarthur <nickarthur.night@gmail.com>
2020-10-25 22:49:16 -0500

    1. Added View Controller Creation Methods 2. Added TabBar  Green Tint

--- GHFollowerrs/FavoritesListVC.swift
+++ GHFollowerrs/FavoritesListVC.swift
@@ -12,7 +12,12 @@ class FavoritesListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
+        
+        
     }
+    
+    
+    
     
 
 
--- GHFollowerrs/SceneDelegate.swift
+++ GHFollowerrs/SceneDelegate.swift
@@ -13,16 +13,43 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
 
+
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
         guard let windowScene = (scene as? UIWindowScene) else { return }
-
-        window = UIWindow(frame: windowScene.coordinateSpace.bounds)  // create a window that fills the screen
-        window?.windowScene = windowScene // keep a reference in our window
-        window?.rootViewController = UITabBarController()
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = createTabBarController()
         window?.makeKeyAndVisible()
     }
 
+    
+    func createSearchNC() -> UINavigationController {
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    
+    func creatFavoritesNC() -> UINavigationController {
+        let favoritesListVC = FavoritesListVC()
+        favoritesListVC.title = "Favorites"
+        favoritesListVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        return UINavigationController(rootViewController: favoritesListVC)
+    }
+    
+    
+    func createTabBarController() -> UITabBarController {
+        let tabBar = UITabBarController()
+        UITabBar.appearance().tintColor = .systemGreen
+        tabBar.viewControllers = [createSearchNC(),creatFavoritesNC()]
+        return tabBar
+    }
+    
+    
     func sceneDidDisconnect(_ scene: UIScene) {
 
     }
