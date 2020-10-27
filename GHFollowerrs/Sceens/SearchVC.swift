0e51d11e586f0914d0a67fc15247467a327cefee
nickarthur <nickarthur.night@gmail.com>
2020-10-27 00:12:15 -0500

    1. Added content to Search View, logoImageView, userNameTextField, callToActionButton 2. Laid out the Github Logo Image View using AutoLayout

--- GHFollowerrs/Sceens/SearchVC.swift
+++ GHFollowerrs/Sceens/SearchVC.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class SearchVC: UIViewController {
 
+    
+    let logoImageView = UIImageView()
+    let userNameTextField = GFTextField(frame: .zero)
+    let callToActionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemBackground // white/black based on light/dark mode
+        configureLogoImageView()
     }
     
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    private func configureLogoImageView() {
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        #warning("STRINGLY TYPED! -> gh-logo <- change to constant")
+        logoImageView.image = UIImage(named: "gh-logo")! // TODO: "stringly-typed" refactor to constant
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+    }
 }
