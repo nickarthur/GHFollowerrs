8ac058a58ce806bab00a2fcfb4bd966f956fd699
nickarthur <nickarthur.night@gmail.com>
2020-10-28 01:14:49 -0500

    1. Added Custom GFBodyLabel class

---
+++ GHFollowerrs/Custom Views/GFBodyLabel.swift
@@ -0,0 +1,35 @@
+//
+//  GFBodyLabel.swift
+//  GHFollowerrs
+//
+//  Created by Larry Nickerson on 10/28/20.
+//
+
+import UIKit
+
+class GFBodyLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(textAlignment: NSTextAlignment) {
+        self.init(frame: .zero)
+        self.textAlignment = textAlignment
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        font = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.75
+        lineBreakMode = .byWordWrapping
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
