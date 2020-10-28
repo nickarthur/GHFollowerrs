3399586a06ffa2ae9660ad0f2b7394c21d07dc61
nickarthur <nickarthur.night@gmail.com>
2020-10-28 01:01:44 -0500

    1. Added Custom GFTitleLabel class

---
+++ GHFollowerrs/Custom Views/GFTitleLabel.swift
@@ -0,0 +1,34 @@
+//
+//  GFTitleLabel.swift
+//  GHFollowerrs
+//
+//  Created by Larry Nickerson on 10/28/20.
+//
+
+import UIKit
+
+class GFTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        self.init(frame: .zero)
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.90
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
