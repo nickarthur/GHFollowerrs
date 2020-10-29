ab33a49e61422d50c7d9c5ef2a43050527dc9df8
nickarthur <nickarthur.night@gmail.com>
2020-10-28 23:01:31 -0500

    1. BUGFIX:  Navagation Bar Not reappearing when swiping  Followers Screen from left edge and returning to the SearchVC 2. Renamed Screen Group/Folder

--- GHFollowerrs/Screens/FollwersListVC.swift
+++ GHFollowerrs/Screens/FollwersListVC.swift
@@ -14,6 +14,11 @@ class FollwersListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
 
