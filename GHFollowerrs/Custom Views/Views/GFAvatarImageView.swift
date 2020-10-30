d7f9281466ce72ce97a6202ce549647e82146fc7
nickarthur <nickarthur.night@gmail.com>
2020-10-30 01:40:20 -0500

    1. added initializer to GFImageView 2. also added configure method

--- GHFollowerrs/Custom Views/Views/GFAvatarImageView.swift
+++ GHFollowerrs/Custom Views/Views/GFAvatarImageView.swift
@@ -9,5 +9,18 @@ import UIKit
 
 class GFAvatarImageView: UIImageView {
 
-
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        
+    }
 }
