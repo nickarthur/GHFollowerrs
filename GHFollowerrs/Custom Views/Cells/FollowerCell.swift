5642e40552698365d1301bd03a6ab5bbbe25cd6e
nickarthur <nickarthur.night@gmail.com>
2020-10-30 01:33:52 -0500

    1. Follower cell has - a square UIImageView - title label

--- GHFollowerrs/Custom Views/Cells/FollowerCell.swift
+++ GHFollowerrs/Custom Views/Cells/FollowerCell.swift
@@ -5,8 +5,14 @@
 //  Created by Larry Nickerson on 10/30/20.
 //
 
+
 import UIKit
 
 class FollowerCell: UICollectionViewCell {
     static let reuseID = "FollowerCell"
+    
+    let avatarImageView = UIImageView()
+    let userLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
+
+    
 }
