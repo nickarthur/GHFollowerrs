782630200ee7faf90d11edd374ea0d59c15b829c
nickarthur <nickarthur.night@gmail.com>
2020-11-03 01:28:10 -0600

    1. refactored createThreeColunmFlowLayout into a UIHelper struct

--- GHFollowerrs/Screens/FollowersListVC.swift
+++ GHFollowerrs/Screens/FollowersListVC.swift
@@ -43,7 +43,7 @@ class FollowersListVC: UIViewController {
     
     private func configureCollectionView() {
         collectionView = UICollectionView(frame: view.bounds,
-                                          collectionViewLayout: createThreeColumnFlowLayout())
+                                          collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
 
         collectionView.backgroundColor  = .systemBackground
         collectionView.register(FollowerCell.self,
@@ -58,20 +58,6 @@ class FollowersListVC: UIViewController {
             collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
-    }
-    
-    
-    private func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
-        let width                       = view.bounds.width
-        let padding: CGFloat            = 12
-        let minimumItemSpacing: CGFloat = 10
-        let availableWidth              = width - (padding * 2) - (minimumItemSpacing * 2)
-        let itemWidth                   = availableWidth / 3
-        
-        let flowLayout          = UICollectionViewFlowLayout()
-        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-        flowLayout.itemSize     = CGSize(width: itemWidth, height: itemWidth + 40) // add space for label
-        return flowLayout
     }
     
     
